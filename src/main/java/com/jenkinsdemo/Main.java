7381f766acbb73db948b10401e60c83cc77d724c
Krerk Piromsopa <Krerk.p@chula.ac.th>
2017-09-20 19:10:48 +0700

    - init maven project

---
+++ src/main/java/com/jenkinsdemo/Main.java
@@ -0,0 +1,14 @@
+package com.jenkinsdemo;
+
+/**
+ *
+ * @author Krerk Piromsopa, Ph.D. <Krerk.P@Chula.ac.th>
+ */
+public class Main {
+    
+    
+    public static void main(String args[]) {
+        System.out.println("Hello, World!");
+    }
+    
+}
